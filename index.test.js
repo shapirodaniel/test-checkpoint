beb1ce284b67863375abfdd2d7ec07bc29e8a850
shapirodaniel <shapirodanieladam@gmail.com>
2022-01-15 06:26:47 -0600

    init commit

---
+++ index.test.js
@@ -0,0 +1,22 @@
+const { add, subtract, multiply, divide } = require('./index');
+
+describe('functions', () => {
+  const x = 10,
+    y = 20;
+
+  test('add', () => {
+    expect(add(x, y)).toBe(30);
+  });
+
+  test('subtract', () => {
+    expect(subtract(x, y)).toBe(-10);
+  });
+
+  test('multiply', () => {
+    expect(multiply(x, y)).toBe(200);
+  });
+
+  test('divide', () => {
+    expect(divide(x, y)).toBe(0.5);
+  });
+});
