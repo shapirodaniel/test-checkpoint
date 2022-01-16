28fc43b418d1c106b340e5103f1547642065063c
shapirodaniel <shapirodanieladam@gmail.com>
2022-01-15 23:05:03 -0600

    refactor: jwt script

--- .github/workflows/jwt.sh
+++ .github/workflows/jwt.sh
@@ -1,32 +1,8 @@
 #!/bin/sh
 
-secret=${env.GRADE_SECRET}
-id=${env.GITHUB_NAME}
-sha=${env.GITHUB_SHA}
-
-header=$( jq --null-input \
-    --arg typ "JWT" \
-    --arg alg "HS256" \
-    '{"typ":$typ,"alg":$alg}')
-
-header=$(
-    echo "${header}" | jq --arg time_str "$(date +%s)" \
-    '
-    ($time_str | tonumber) as $time_num
-    | .iat=$time_num
-    | .exp=($time_num + 300)
-    '
-)
-
-payload=$(jq --null-input \
-    --arg id "${id}" \
-    --arg sha "${sha}" \
-    '{"id": $id, "sha": $sha}')
-
 base64_encode()
 {
     declare input=${1:-$(</dev/stdin)}
-    # Use `tr` to URL encode the output from base64.
     printf '%s' "${input}" | base64 | tr -d '=' | tr '/+' '_-' | tr -d '\n'
 }
 
@@ -41,10 +17,26 @@ hmacsha256_sign()
     printf '%s' "${input}" | openssl dgst -binary -sha256 -hmac "${secret}"
 }
 
+secret=${env.GRADE_SECRET}
+id=${env.GITHUB_NAME}
+sha=${env.GITHUB_SHA}
+iat="$(date +%s)"
+exp=$(echo "$iat + 300" | bc)
+
+header=$( jq --null-input \
+    --arg typ "JWT" \
+    --arg alg "HS256" \
+    --arg iat "${iat}" \
+    --arg exp "${exp}" \
+    '{"typ":$typ,"alg":$alg,"iat":$iat,"exp":$exp}')
+
+payload=$(jq --null-input \
+    --arg id "${id}" \
+    --arg sha "${sha}" \
+    '{"id": $id, "sha": $sha}')
+
 header_base64=$(echo "${header}" | json | base64_encode)
 payload_base64=$(echo "${payload}" | json | base64_encode)
-
-header_payload=$(echo "${header_base64}.${payload_base64}")
 signature=$(echo "${header_payload}" | hmacsha256_sign | base64_encode)
 
-echo "${header_payload}.${signature}"
+echo "${header_base64}.${payload_base64}.${signature}"
