c314de9d6f91b0cffe4466b0d1d66d3377c8b597
shapirodaniel <shapirodanieladam@gmail.com>
2022-01-15 21:17:08 -0600

    add: access token logic

---
+++ .github/workflows/jwt.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+secret=env.JWT_SECRET
+id=env.GITHUB_NAME
+
+header='{
+    "typ": "JWT",
+    "alg": "HS256",
+}'
+
+header=$(
+    echo "${header}" | jq --arg time_str "$(date +%s)" \
+    '
+    ($time_str | tonumber) as $time_num
+    | .iat=$time_num
+    | .exp=($time_num + 300)
+    '
+)
+payload=<<EOF
+{
+    "id": "${id}"
+}
+EOF
+
+base64_encode()
+{
+    declare input=${1:-$(</dev/stdin)}
+    # Use `tr` to URL encode the output from base64.
+    printf '%s' "${input}" | base64 | tr -d '=' | tr '/+' '_-' | tr -d '\n'
+}
+
+json() {
+    declare input=${1:-$(</dev/stdin)}
+    printf '%s' "${input}" | jq -c .
+}
+
+hmacsha256_sign()
+{
+    declare input=${1:-$(</dev/stdin)}
+    printf '%s' "${input}" | openssl dgst -binary -sha256 -hmac "${secret}"
+}
+
+header_base64=$(echo "${header}" | json | base64_encode)
+payload_base64=$(echo "${payload}" | json | base64_encode)
+
+header_payload=$(echo "${header_base64}.${payload_base64}")
+signature=$(echo "${header_payload}" | hmacsha256_sign | base64_encode)
+
+echo "${header_payload}.${signature}"
